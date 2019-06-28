21a0f46e9ddffa3253638dcc90db5ed9e3ddde12
HarD-BR <guicandido1337@hotmail.com>
2019-06-28 16:47:04 -0300

    Alterado nome da variável criada para exibição de evento

--- funcphp/deletar_pedido.php
+++ funcphp/deletar_pedido.php
@@ -7,7 +7,7 @@ $codigo	= $_POST["bt_del"];
 $sql = "UPDATE pedidos SET status='Inativo' WHERE id='$codigo'";
 
 if ($con->query($sql) === TRUE) {
-   $_SESSION['pedidodeletado'] = "true";
+   $_SESSION['pedidodesativado'] = "true";
     header("Location: ../menu_search.php");
 } else {
     echo "Error: " . $sql . "<br>" . $con->error;
