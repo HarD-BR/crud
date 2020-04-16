2f16e35d669a77352806aa53e0268d5031e7c9b1
HarD-BR <guicandido1337@hotmail.com>
2020-04-16 11:32:06 -0300

    Arrumando redirecionamento

--- funcphp/lg.php
+++ funcphp/lg.php
@@ -1,27 +1,27 @@
 <?php
-    session_start();
-    include 'conn.php';
+session_start();
+include 'conn.php';
 
-    if ((isset($_POST['user_txt'])) && (isset($_POST['pass_txt']))) {
-        $sql_query = 'SELECT * FROM usuario WHERE usuario = :USUARIO && senha = :SENHA';
+if ((isset($_POST['user_txt'])) && (isset($_POST['pass_txt']))) {
+    $sql_query = 'SELECT * FROM usuario WHERE usuario = :USUARIO && senha = :SENHA';
 
-        $stmt = $con->prepare($sql_query);
-        $stmt->bindParam(':USUARIO', $usuario);
-        $stmt->bindParam(':SENHA', $senha);
+    $stmt = $con->prepare($sql_query);
+    $stmt->bindParam(':USUARIO', $usuario);
+    $stmt->bindParam(':SENHA', $senha);
 
-        $usuario = $_POST['user_txt'];
-        $senha = $_POST['pass_txt'];
+    $usuario = $_POST['user_txt'];
+    $senha = $_POST['pass_txt'];
 
-        $stmt->execute();
-        $resultado = $stmt->fetch();
+    $stmt->execute();
+    $resultado = $stmt->fetch();
 
-        if (isset($resultado)) {
-            $_SESSION['usuarioId'] = $resultado['id_usuario'];
-            $_SESSION['usuarioNome'] = $resultado['usuario'];
-            $_SESSION['usuarioEmail'] = $resultado['email'];
-            header('Location: ../menu_search.php');
-        } else {
-            $_SESSION['nao_login'] = '1';
-            header('Location: ../index.php');
-        }
+    if (isset($resultado)) {
+        $_SESSION['usuarioId'] = $resultado['id_usuario'];
+        $_SESSION['usuarioNome'] = $resultado['usuario'];
+        $_SESSION['usuarioEmail'] = $resultado['email'];
+        header('Location: ../index.php');
+    } else {
+        $_SESSION['nao_login'] = '1';
+        header('Location: ../login.php');
     }
+}
